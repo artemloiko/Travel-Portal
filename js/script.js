af53aec5888f1a2a2930212791499644052b7889
Artem <33007769+artuom130@users.noreply.github.com>
2018-05-21 22:41:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict"
+// Боковое меню по кнопке
+var toggle = document.querySelector('.nav-toggle');
+var toggled = document.querySelector('.right-sidebar');
+toggle.addEventListener('click', function(e) {
+	this.classList.toggle('opened');
+	toggled.classList.toggle('openedBar');
+});
+var inputBtn = document.getElementsByClassName('right-sidebar-search__field');
+
+// Кнопка наверх
+
+var toTop = document.getElementById('btnToTop');
+function scrollToTop() {
+	window.pageYOffset > 2000 ? window.scrollBy(0, -100) : window.pageYOffset > 1000 ? window.scrollBy(0, -60) : window.scrollBy(0, -40);
+	if (window.pageYOffset) setTimeout(scrollToTop, 10);
+}
+toTop.addEventListener('click', scrollToTop);
+var content = document.querySelector('.content');
+content.addEventListener('click', function () {
+	if (toggled.classList.contains('openedBar')) {
+		toggled.classList.toggle('openedBar');
+	}
+});
+
+// Боковое меню свайпом
+
+var xStart, yStart;
+var body = document.querySelector('body');
+body.addEventListener('touchstart', function (e) {
+	xStart = e.changedTouches[0].screenX;
+	yStart = e.changedTouches[0].screenY;
+});
+body.addEventListener('touchend', function (e) {
+	var touches = e.changedTouches;
+	var xEnd = touches[0].screenX;
+	var yEnd = touches[0].screenY;
+
+	if (xStart - xEnd > 50) toggled.classList.toggle('openedBar');
+	if (xStart - xEnd < -50) toggled.classList.toggle('openedBar');
+});
+
+// Календарь
+
+var calendar = document.getElementById('date');
+var today = new Date();
+var months = ['ЯНВ', 'ФЕВ', 'МАР', 'АПР', 'МАЙ', 'ИЮН', 'ИЮЛ', 'АВГ', 'СЕН', 'ОКТ', 'НОЯ', 'ДЕК'];
+calendar.innerHTML = `${months[today.getMonth()]}<br><b>${today.getDate()}</b>`;
